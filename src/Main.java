25156ebf4597332bbf01fe92351e9f3a504b93d9
Aleksandra Makarova <9163332265@mail.ru>
2023-03-01 12:18:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 50_000;
+        int oneBonus = 20;
+        int totalBonus = price / oneBonus;
+        System.out.println(totalBonus);
+
+    }
+}
